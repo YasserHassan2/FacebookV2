79bbb0ff6405912102711b723def196a9829a5a2
Yasser <yasserhassan.it.asd>
2020-01-26 21:55:05 +0200

    first commit

---
+++ app/src/main/java/com/yasser/facebookv2/ui/main/PostViewModel.java
@@ -0,0 +1,36 @@
+package com.yasser.facebookv2.ui.main;
+
+import android.widget.Toast;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.yasser.facebookv2.data.PostsClient;
+import com.yasser.facebookv2.pojo.PostModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class PostViewModel  extends ViewModel {
+
+    MutableLiveData<List<PostModel>> postsMutableLiveData = new MutableLiveData<>();
+
+
+    public void getPosts(){
+        PostsClient.getINSTANCE().getPosts().enqueue(new Callback<List<PostModel>>() {
+            @Override
+            public void onResponse(Call<List<PostModel>> call, Response<List<PostModel>> response) {
+                postsMutableLiveData.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<PostModel>> call, Throwable t) {
+
+
+            }
+        });
+    }
+}
